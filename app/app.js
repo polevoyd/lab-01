0d6e38629678d9006950bae30a0204e1487fe1a1
Dmitry <manxxeta@gmail.com>
2018-09-11 12:15:18 -0700

    first

--- app/app.js
+++ app/app.js
@@ -1,14 +1,14 @@
 
-const divBoxA = document.querySelector('div.a');
-const divBoxB = document.querySelector('div.b');
-const divBoxC = document.querySelector('div.c');
-const divBoxD = document.querySelector('div.d');
-const divBoxE = document.querySelector('div.e');
-const divBoxF = document.querySelector('div.f');
-const divBoxG = document.querySelector('div.g');
-const divBoxH = document.querySelector('div.h');
-const divBoxI = document.querySelector('div.i');
+// const divBoxA = document.querySelector('div.a');
+// const divBoxB = document.querySelector('div.b');
+// const divBoxC = document.querySelector('div.c');
+// const divBoxD = document.querySelector('div.d');
+// const divBoxE = document.querySelector('div.e');
+// const divBoxF = document.querySelector('div.f');
+// const divBoxG = document.querySelector('div.g');
+// const divBoxH = document.querySelector('div.h');
+// const divBoxI = document.querySelector('div.i');
 
-const divBoxes = [divBoxA, divBoxB, divBoxC, divBoxD, divBoxE, divBoxF, divBoxG, divBoxH, divBoxI];
-const colors = ['#069E2D', '#058E3F', '#04773B', '#036016', '#03440C', '#8DD29F', '#49AC73', '#76B494', '#75A87F', '#47774E', '#401F3E', '#3F2E56', '#453F78', '#FAF2A1'];
+// const divBoxes = [divBoxA, divBoxB, divBoxC, divBoxD, divBoxE, divBoxF, divBoxG, divBoxH, divBoxI];
+// const colors = ['#069E2D', '#058E3F', '#04773B', '#036016', '#03440C', '#8DD29F', '#49AC73', '#76B494', '#75A87F', '#47774E', '#401F3E', '#3F2E56', '#453F78', '#FAF2A1'];
 
